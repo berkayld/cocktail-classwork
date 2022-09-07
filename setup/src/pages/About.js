2e780dcaa9c68a57a31891e56a96700f9331904d
berkayld <brkyyildirim0@gmail.com>
2022-09-08 00:33:10 +0300

    classwork done

---
+++ setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptates, quae, quod, voluptate quibusdam voluptatem quia voluptas
+        doloremque quidem quos natus. Quisquam, quae. Quisquam, quae. Quisquam,
+      
+      </p>
+    </section>
+                  
+  )
+}
+
+export default About
